1bddaf4db7780472a8e77ed5b70aec68994e4f2d
norvalbv <benjinorval@gmail.com>
2023-07-01 11:51:33 +0100

    update to loader

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,9 +1,13 @@
+import Loader from 'components/Loader';
 import React from 'react';
 import { useWeather } from 'services';
 
 const Landing = () => {
   const { data } = useWeather({ lat: 10, lon: 10 });
   console.log(data);
+
+  if (!data) return <Loader />;
+
   return <div>Landing</div>;
 };
 
