8553cf7edc8b81b5dc3c4aa5cfd640140e6ef543
norvalbv <benjinorval@gmail.com>
2023-07-01 19:59:53 +0100

    add minimal layout

---
+++ src/layout/MinimalLayout/index.tsx
@@ -0,0 +1,12 @@
+import React, { ReactElement } from 'react';
+import { Outlet } from 'react-router-dom';
+
+const MinimalLayout = (): ReactElement => {
+  return (
+    <>
+      <Outlet />
+    </>
+  );
+};
+
+export default MinimalLayout;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,16 @@
 import React, { Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
-import MainLayout from 'layout';
+import MainLayout from 'layout/MainLayout';
 import Landing from 'pages/Landing';
 import NotFound from 'pages/NotFound';
+import MinimalLayout from 'layout/MinimalLayout';
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
       <Route path="/" element={<MainLayout />}>
         <Route path="home" element={<Landing />} />
+      </Route>
+      <Route path="/" element={<MinimalLayout />}>
         <Route path="*" element={<NotFound />} />
       </Route>
     </Route>
