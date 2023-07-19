7530ea08954117f2a684655e733376598b875436
norvalbv <benjinorval@gmail.com>
2023-07-19 20:57:28 +0100

    add snapshots

--- src/components/Loader/__tests__/Loader.test.tsx
+++ src/components/Loader/__tests__/Loader.test.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 
+import { ThemeContextProvider } from 'context/theme';
+
 import Loader from '..';
 
 describe('<Loader />', () => {
   test('Should render correctly with mandatory props', () => {
-    const tree = renderer.create(<Loader />).toJSON();
+    const tree = renderer
+      .create(
+        <ThemeContextProvider>
+          <Loader />
+        </ThemeContextProvider>
+      )
+      .toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
